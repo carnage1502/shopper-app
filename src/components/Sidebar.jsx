a62f1425769fadddff0ac190ea44267920e27cf1
carnage1502 <shivamsanjay152@gmail.com>
2024-03-20 13:33:10 +0530

    completed  sidebar component and sidebar context

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,12 @@
+import { Link } from "react-router-dom";
+import { IoMdArrowForward } from "react-icons/io";
+import { FiTrash2 } from "react-icons/fi";
+import CartItem from "../components/CartItem";
+import { SidebarContext } from "../contexts/SidebarContext";
+import { useContext } from "react";
+
 const Sidebar = () => {
+  const { isOpen, handleClose } = useContext(SidebarContext);
   return <div>Sidebar</div>;
 };
 
