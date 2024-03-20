9b2b6cdff111052b2b0079297e7ef553ccb3098a
carnage1502 <shivamsanjay152@gmail.com>
2024-03-21 01:40:42 +0530

    complete header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,16 @@
+import { useContext } from "react";
+import { SidebarContext } from "../contexts/SidebarContext";
+import { BsBag } from "react-icons/bs";
 const Header = () => {
-  return <div>Header</div>;
+  const { isOpen, setIsOpen } = useContext(SidebarContext);
+  return (
+    <header className="bg-pink-200 ">
+      <div>Header</div>
+      <div onClick={() => setIsOpen(!isOpen)}>
+        <BsBag className="text-2xl cursor-pointer flex relative" />
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,7 +7,23 @@ import { useContext } from "react";
 
 const Sidebar = () => {
   const { isOpen, handleClose } = useContext(SidebarContext);
-  return <div>Sidebar</div>;
+  return (
+    <div
+      className={`${
+        isOpen ? "right-0" : "-right-full"
+      } w-full bg-white fixed top-0 h-full shadow-2xl md:w-[35vw] xl:max-w-[30vw] transition-all duration-300 z-20 px-4 lg:px-[35px]`}
+    >
+      <div className="flex items-center justify-between py-6 border-b">
+        <div className="text-sm uppercase font-semibold">Shopping Bag (0)</div>
+        <div
+          onClick={handleClose}
+          className="cursor-pointer w-8 h-8 flex justify-center items-center"
+        >
+          <IoMdArrowForward className="text-2xl" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
