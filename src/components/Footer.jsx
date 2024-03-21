01fc5529e36dac83bf8326de0d6b509927b739c8
carnage1502 <shivamsanjay152@gmail.com>
2024-03-21 16:19:37 +0530

    completed footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,13 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="bg-primary py-10">
+      <div className="container mx-auto">
+        <p className="text-white text-center">
+          Copyright &copy; Shopper App 2024 by Carnage. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
